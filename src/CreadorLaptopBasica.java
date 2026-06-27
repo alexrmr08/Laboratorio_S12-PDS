6b2b5877610185a07a9751cc1a5dcba7cc5ef741
alexrmr08 <walter.romero123.2@gmail.com>
2026-06-27 17:07:03 -0500

    feat: Implementar la clase CreadorLpatopBasica

---
+++ src/CreadorLaptopBasica.java
@@ -0,0 +1,16 @@
+public class CreadorLaptopBasica extends LaptopCreador {
+ 
+    @Override
+    public Laptop crearLaptop() {
+
+        return new LaptopBasica(
+                "Lenovo",
+                "IdeaPad 3",
+                2024,
+                "Intel Core i3",
+                15
+        );
+
+    }
+
+}
