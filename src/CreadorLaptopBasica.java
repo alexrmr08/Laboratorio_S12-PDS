44d3522d0aee178bf3bd6eff6ebe03fb70f18a90
alexrmr08 <walter.romero123.2@gmail.com>
2026-06-27 17:51:23 -0500

    refactor: Simplificar la lógica a una sola línea.

--- src/CreadorLaptopBasica.java
+++ src/CreadorLaptopBasica.java
@@ -3,13 +3,7 @@ public class CreadorLaptopBasica extends LaptopCreador {
     @Override
     public Laptop crearLaptop() {
 
-        return new LaptopBasica(
-                "Lenovo",
-                "IdeaPad 3",
-                2024,
-                "Intel Core i3",
-                15
-        );
+        return new LaptopBasica("Lenovo", "IdeaPad 3", 2024, "Intel Core i3", 15);
 
     }
 
